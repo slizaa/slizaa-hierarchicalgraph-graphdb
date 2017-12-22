5d28f112be1e5efb66fc22faac9803d3b865f1e9
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2017-12-22 11:49:58 +0100

    fixed upload.sh

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -5,7 +5,7 @@ target_dir=slizaa-hierarchicalgraph-graphdb
 
 echo TRAVIS_BUILD_DIR: $TRAVIS_BUILD_DIR
 
-PRODUCTS_DIR=$TRAVIS_BUILD_DIR/releng/org.slizaa.hierarchicalgraph.core.p2/target/repository
+PRODUCTS_DIR=$TRAVIS_BUILD_DIR/releng/org.slizaa.hierarchicalgraph.graphdb.p2/target/repository
 echo PRODUCTS_DIR: $PRODUCTS_DIR
 
 filesToUpload=$(find $PRODUCTS_DIR -name "*.jar" -printf '%P\n')
